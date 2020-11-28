4f76f2c3fda8064e5ec36ac037a67babd6ab198d
Ankurjyoti Das <68739425+AnkurJDas171@users.noreply.github.com>
2020-11-28 21:03:14 +0530

    try catch removed

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
 
-function App(){
+function App() {
   const [formDetail, setFormDetails] = React.useState({
     errorMessage: "",
     username: null,
     noError: false,
-    gender: "male"
+    name: "",
+    email: "",
+    gender: "male",
+    phoneNumber: "",
+    password: ""
   });
 
   const checkForAlphanumeric = (name) => {
@@ -23,17 +27,7 @@ function App(){
   };
 
   const getUsrename = (formObj) => {
-    console.log("username generator called");
-    console.log(formObj.email);
-    let username = "";
-    let email = formObj.email;
-    if (email) {
-      let i = 0;
-      while (email[i] !== "@") {
-        username += email[i];
-        i++;
-      }
-    }
+    let username = formObj.email.split("@")[0];
     console.log(username);
     return username;
   };
@@ -48,53 +42,47 @@ function App(){
     let length = Object.keys(formDetail).length;
     let newFormDetail = { ...formDetail };
 
-    try {
-      if (length < 8) {
-        newFormDetail.noError = false;
-        throw "All fields are mandatory";
-      } else if (checkForAlphanumeric(formDetail.name)) {
-        newFormDetail.noError = false;
-        console.log("error 2");
-        throw "Name is not alphanumeric";
-      } else if (
-        formDetail.gender !== "male" &&
-        formDetail.gender !== "female" &&
-        formDetail.gender !== "other"
-      ) {
-        newFormDetail.noError = false;
-        throw "Please identify as male, female or others";
-      } else if (isNaN(formDetail.phoneNumber)) {
-        newFormDetail.noError = false;
-        throw "Phone Number must contain only numbers";
-      } else if (formDetail.password.length < 6) {
-        newFormDetail.noError = false;
-        throw "Password must contain atleast 6 letters";
-      } else {
-        newFormDetail.noError = true;
-        newFormDetail.errorMessage = null;
-      }
-    } catch (err) {
-      newFormDetail.errorMessage = `Error Message: ${err}`;
-    } finally {
-      newFormDetail.username = getUsrename(formDetail);
-
-      setFormDetails(newFormDetail);
+    if (length < 8) {
+      newFormDetail.noError = false;
+      newFormDetail.errorMessage = "All fields are mandatory";
+    } else if (checkForAlphanumeric(formDetail.name)) {
+      newFormDetail.noError = false;
+      console.log("error 2");
+      newFormDetail.errorMessage = "Name is not alphanumeric";
+    } else if (
+      formDetail.gender !== "male" &&
+      formDetail.gender !== "female" &&
+      formDetail.gender !== "other"
+    ) {
+      newFormDetail.noError = false;
+      newFormDetail.errorMessage = "Please identify as male, female or others";
+    } else if (isNaN(formDetail.phoneNumber)) {
+      newFormDetail.noError = false;
+      newFormDetail.errorMessage = "Phone Number must contain only numbers";
+    } else if (formDetail.password.length < 6) {
+      newFormDetail.noError = false;
+      newFormDetail.errorMessage = "Password must contain atleast 6 letters";
+    } else {
+      newFormDetail.noError = true;
+      newFormDetail.errorMessage = null;
     }
+
+    newFormDetail.username = getUsrename(formDetail);
+
+    setFormDetails(newFormDetail);
   };
 
   return (
     <div id="main">
-      
-      {formDetail.noError && <div>Hello {formDetail.username}</div>}
-      {!formDetail.noError && <div>{formDetail.errorMessage}</div>}
       {!formDetail.noError && (
-        <form className="formContainer">
+        <div className="formContainer">
           <label className="label" for="name">
             Name
           </label>
           <input
             type="text"
             data-testid="name"
+            value={formDetail.name}
             onChange={(event) => handelValueChange(event.target.value, "name")}
           />
 
@@ -104,13 +92,14 @@ function App(){
           <input
             type="text"
             data-testid="email"
+            value={formDetail.email}
             onChange={(event) => handelValueChange(event.target.value, "email")}
           />
 
           <label className="label" for="gender">
             gender
           </label>
-          <select
+          {/* <select
             default={formDetail.gender}
             onChange={(event) =>
               handelValueChange(event.target.value, "gender")
@@ -120,8 +109,15 @@ function App(){
             <option value="male">male</option>
             <option value="female">female</option>
             <option value="other">other</option>
-          </select>
-          {/* <input type="text" default={formDetail.gender} data-testid = 'gender' /> */}
+          </select> */}
+          <input
+            type="text"
+            value={formDetail.gender}
+            data-testid="gender"
+            onChange={(event) =>
+              handelValueChange(event.target.value, "gender")
+            }
+          />
 
           <label className="label" for="phoneNumber">
             Phone Number
@@ -129,6 +125,7 @@ function App(){
           <input
             type="text"
             data-testid="phoneNumber"
+            value={formDetail.phoneNumber}
             onChange={(event) =>
               handelValueChange(event.target.value, "phoneNumber")
             }
@@ -140,6 +137,7 @@ function App(){
           <input
             type="password"
             data-testid="password"
+            value={formDetail.password}
             onChange={(event) =>
               handelValueChange(event.target.value, "password")
             }
@@ -148,12 +146,13 @@ function App(){
           <button data-testid="submit" onClick={handelClick}>
             Submit
           </button>
-        </form>
+        </div>
       )}
+
+      {formDetail.noError && <div>Hello {formDetail.username}</div>}
+      {!formDetail.noError && <div>{formDetail.errorMessage}</div>}
     </div>
   );
-};
+}
 
 export default App;
-
-
