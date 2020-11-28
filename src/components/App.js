93ca065b367dda44baf422ff70b68a502bf8a065
Ankurjyoti Das <68739425+AnkurJDas171@users.noreply.github.com>
2020-11-28 20:37:39 +0530

    changes made

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,35 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
-
-  const [formDetail, setFormDetails] = useState({
-    errorMessage:"",
+  const [formDetail, setFormDetails] = React.useState({
+    errorMessage: "",
     username: null,
     noError: false,
     gender: "male"
   });
 
-  const checkForAlphanumeric=(name)=>{
-
-    let alphaNumericCharacters = "1234567890qwertyuiopasdfghjklzxcvbnm QWERTYUIOPASDFGHJKLZXCVBNM";
-    let i=0;
-    while(i< name.length){
-      if(!alphaNumericCharacters.includes(name[i])){
+  const checkForAlphanumeric = (name) => {
+    let alphaNumericCharacters =
+      "1234567890qwertyuiopasdfghjklzxcvbnm QWERTYUIOPASDFGHJKLZXCVBNM";
+    let i = 0;
+    while (i < name.length) {
+      if (!alphaNumericCharacters.includes(name[i])) {
         return true;
       }
       i++;
     }
     return false;
-
   };
 
-  const getUsrename=(formObj)=>{
-
+  const getUsrename = (formObj) => {
     console.log("username generator called");
     console.log(formObj.email);
     let username = "";
-    let email = formObj.email; 
-    if(email){
-      let i=0;
-      while(email[i] !== "@"){
+    let email = formObj.email;
+    if (email) {
+      let i = 0;
+      while (email[i] !== "@") {
         username += email[i];
         i++;
       }
@@ -49,7 +46,7 @@ const App = () => {
 
   const handelClick = () => {
     let length = Object.keys(formDetail).length;
-    let newFormDetail = {...formDetail};
+    let newFormDetail = { ...formDetail };
 
     try {
       if (length < 8) {
@@ -57,113 +54,110 @@ const App = () => {
         throw "All fields are mandatory";
       } else if (checkForAlphanumeric(formDetail.name)) {
         newFormDetail.noError = false;
+        console.log("error 2");
         throw "Name is not alphanumeric";
-      } 
-      else if(formDetail.gender !== "male" && formDetail.gender !== "female" && formDetail.gender !== "other"){
+      } else if (
+        formDetail.gender !== "male" &&
+        formDetail.gender !== "female" &&
+        formDetail.gender !== "other"
+      ) {
         newFormDetail.noError = false;
         throw "Please identify as male, female or others";
-      }
-      else if(isNaN(formDetail.phoneNumber)){
+      } else if (isNaN(formDetail.phoneNumber)) {
         newFormDetail.noError = false;
         throw "Phone Number must contain only numbers";
-      }
-      else if(formDetail.password.length < 6){
+      } else if (formDetail.password.length < 6) {
         newFormDetail.noError = false;
         throw "Password must contain atleast 6 letters";
-      }
-      else{
+      } else {
         newFormDetail.noError = true;
         newFormDetail.errorMessage = null;
       }
-
-
     } catch (err) {
-
       newFormDetail.errorMessage = `Error Message: ${err}`;
-
-    }finally{
-
-      
+    } finally {
       newFormDetail.username = getUsrename(formDetail);
-      
+
       setFormDetails(newFormDetail);
-
     }
-
   };
 
   return (
     <div id="main">
+      {formDetail.noError && <div>Hello {formDetail.username}</div>}
+      {!formDetail.noError && <div>{formDetail.errorMessage}</div>}
+      {!formDetail.noError && (
+        <div className="formContainer">
+          <label className="label" for="name">
+            Name
+          </label>
+          <input
+            type="text"
+            data-testid="name"
+            onChange={(event) => handelValueChange(event.target.value, "name")}
+          />
 
-    {formDetail.noError && <div>Hello {formDetail.username}</div>}
-    {!formDetail.noError && <div>{formDetail.errorMessage}</div>}
-    {!formDetail.noError && (
-      <div className="formContainer">
-        <label className="label" for="name">
-          Name
-        </label>
-        <input
-          type="text"
-          data-testid="name"
-          onChange={(event) => handelValueChange(event.target.value, "name")}
-        />
+          <label className="label" for="email">
+            email
+          </label>
+          <input
+            type="text"
+            data-testid="email"
+            onChange={(event) => handelValueChange(event.target.value, "email")}
+          />
 
-        <label className="label" for="email">
-          email
-        </label>
-        <input
-          type="text"
-          data-testid="email"
-          onChange={(event) => handelValueChange(event.target.value, "email")}
-        />
+          <label className="label" for="gender">
+            gender
+          </label>
+          <select
+            default={formDetail.gender}
+            onChange={(event) =>
+              handelValueChange(event.target.value, "gender")
+            }
+            data-testid="gender"
+          >
+            <option value="male">male</option>
+            <option value="female">female</option>
+            <option value="other">other</option>
+          </select>
+          {/* <input type="text" default={formDetail.gender} data-testid = 'gender' /> */}
 
-        <label className="label" for="gender">
-          gender
-        </label>
-        <select
-          default={formDetail.gender}
-          onChange={(event) =>
-            handelValueChange(event.target.value, "gender")
-          }
-          data-testid="gender"
-        >
-          <option value="male">male</option>
-          <option value="female">female</option>
-          <option value="other">other</option>
-        </select>
-        {/* <input type="text" default={formDetail.gender} data-testid = 'gender' /> */}
+          <label className="label" for="phoneNumber">
+            Phone Number
+          </label>
+          <input
+            type="text"
+            data-testid="phoneNumber"
+            onChange={(event) =>
+              handelValueChange(event.target.value, "phoneNumber")
+            }
+          />
 
-        <label className="label" for="phoneNumber">
-          Phone Number
-        </label>
-        <input
-          type="text"
-          data-testid="phoneNumber"
-          onChange={(event) =>
-            handelValueChange(event.target.value, "phoneNumber")
-          }
-        />
+          <label className="label" for="password">
+            password
+          </label>
+          <input
+            type="password"
+            data-testid="password"
+            onChange={(event) =>
+              handelValueChange(event.target.value, "password")
+            }
+          />
 
-        <label className="label" for="password">
-          password
-        </label>
-        <input
-          type="password"
-          data-testid="password"
-          onChange={(event) =>
-            handelValueChange(event.target.value, "password")
-          }
-        />
-
-        <button data-testid="submit" onClick={handelClick}>
-          Submit
-        </button>
-      </div>
-    )}
-
+          <button data-testid="submit" onClick={handelClick}>
+            Submit
+          </button>
+        </div>
+      )}
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
+
+// 1) Name data-testid = 'name'
+// 2) Email address data-testid = 'email'
+// 3) Gender data-testid = 'gender'
+// 3) Phone Number data-testid = 'phoneNumber'
+// 4) Password data-testid = 'password', type='password'
+// 5) Submit button data-testid = 'submit'
